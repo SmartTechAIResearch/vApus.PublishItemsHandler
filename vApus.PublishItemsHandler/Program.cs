4d9b44e14db2464ada5f7d1f423af5f0c43bab75
Dieter Vandroemme <dieter@sizingservers.be>
2016-02-16 11:12:04 +0100

    First commit

---
+++ vApus.PublishItemsHandler/Program.cs
@@ -0,0 +1,67 @@
+﻿/*
+ * Copyright 2009 (c) Sizing Servers Lab
+ * University College of West-Flanders, Department GKG
+ * 
+ * Author(s):
+ *    Dieter Vandroemme
+ */
+using System;
+using System.Reflection;
+using System.Threading;
+
+namespace vApus.PublishItemsHandler {
+    class Program {
+        private static readonly Mutex _namedMutex = new Mutex(true, Assembly.GetExecutingAssembly().FullName);
+        static void Main(string[] args) {
+            if (_namedMutex.WaitOne(0)) {
+                int port = 4337;
+
+                Console.WriteLine("vApus publish items handler");
+                Console.WriteLine("-----");
+                Console.WriteLine("This handler listens for messages on TCP port " + port + " over IPv4 published by vApus and puts them in a standardized vApus MySQL results db.");
+
+                Console.WriteLine("Type MySQL <host>,<port>,<user> and press enter");
+
+                string[] arr = Console.ReadLine().Split(',');
+
+                Console.WriteLine("Type <password> and press enter");
+
+                string password = ReadPassword();
+                try {
+                    PublishItemHandler.Init(arr[0], int.Parse(arr[1]), arr[2], password);
+                    QueuedListener.Start(port);
+                    Console.WriteLine("Listening on TCP port " + port + " over IPv4.");
+                }
+                catch (Exception ex) {
+                    Console.WriteLine("Failed connecting to MySQL\n" + ex.Message);
+                }
+                Console.WriteLine("Press <any key> to quit");
+                Console.ReadLine();
+            }
+        }
+
+        public static string ReadPassword() {
+            string password = string.Empty;
+            ConsoleKeyInfo info = Console.ReadKey(true);
+            while (info.Key != ConsoleKey.Enter) {
+                if (info.Key != ConsoleKey.Backspace) {
+                    Console.Write("*");
+                    password += info.KeyChar;
+                }
+                else if (info.Key == ConsoleKey.Backspace) {
+                    if (!string.IsNullOrEmpty(password)) {
+                        password = password.Substring(0, password.Length - 1);
+                        int pos = Console.CursorLeft;
+                        Console.SetCursorPosition(pos - 1, Console.CursorTop);
+                        Console.Write(" ");
+                        Console.SetCursorPosition(pos - 1, Console.CursorTop);
+                    }
+                }
+                info = Console.ReadKey(true);
+            }
+            Console.WriteLine();
+            return password;
+        }
+    }
+}
+
